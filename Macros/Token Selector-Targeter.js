2c0247efe8f47f71bcd31eef0d19be1d45ac8370
nythz <152181871+nythz@users.noreply.github.com>
2025-04-29 15:04:30 -0400

    Update Token Selector-Targeter.js
    added "Both" choice for the Alliance selection

--- Macros/Token Selector-Targeter.js
+++ Macros/Token Selector-Targeter.js
@@ -1,9 +1,10 @@
-// Macro menu to select or target multiple tokens on the current scene
-// either party of opposition alliances
-// either all on scene or only in combat
-
+// Token Selector/Targeter v2 by Nythz
+// Menu to mass-select or target tokens based on choices of alliance and combat status
+// Ignores Dead or Foundry "Hidden" Tokens
+// Changelog:
+// v2: added "Both" choice for the Alliance selection
+//
 let tokenActionDialog;
-
 function performAction(action, alliance, scope) {
     switch (action) {
         case "select":
@@ -24,7 +25,7 @@ async function handleSelectTokens(targetAlliance, inCombat) {
     await canvas.tokens.releaseAll();
     const tokens = canvas.tokens.placeables.filter(token => {
         const actor = token?.actor;
-        if (!token?.document?.hidden && actor?.isOfType("creature", "hazard", "vehicle") && !actor.isDead && actor?.alliance === targetAlliance) {
+        if (!token?.document?.hidden && actor?.isOfType("creature", "hazard", "vehicle") && !actor.isDead && (targetAlliance === "both" || actor?.alliance === targetAlliance)) {
             if (inCombat) {
                 return game.combat?.combatants.some(c => c.token?.id === token.id);
             }
@@ -37,12 +38,12 @@ async function handleSelectTokens(targetAlliance, inCombat) {
             releaseOthers: false
         });
     }
-    ui.notifications.info(`Selected ${tokens.length} ${targetAlliance} tokens ${inCombat ? 'in combat' : 'on the scene'}.`);
+    ui.notifications.info(`Selected ${tokens.length} ${targetAlliance === 'party' || targetAlliance === 'opposition' ? `${targetAlliance} ` : ''}tokens ${inCombat ? 'in combat' : 'on the scene'}.`);
 }
 async function handleTargetTokens(targetAlliance, inCombat) {
     let tokenIds = canvas.tokens.placeables.filter(token => {
         const actor = token?.actor;
-        if (!token?.document?.hidden && actor?.isOfType("creature", "hazard", "vehicle") && !actor.isDead && actor?.alliance === targetAlliance) {
+        if (!token?.document?.hidden && actor?.isOfType("creature", "hazard", "vehicle") && !actor.isDead && (targetAlliance === "both" || actor?.alliance === targetAlliance)) {
             if (inCombat) {
                 return game.combat?.combatants.some(c => c.token?.id === token.id);
             }
@@ -51,7 +52,7 @@ async function handleTargetTokens(targetAlliance, inCombat) {
         return false;
     }).map(token => token.id);
     game.user.updateTokenTargets(tokenIds);
-    ui.notifications.info(`Targeted ${tokenIds.length} ${targetAlliance} tokens ${inCombat ? 'in combat' : 'on the scene'}.`);
+    ui.notifications.info(`Targeted ${tokenIds.length} ${targetAlliance === 'party' || targetAlliance === 'opposition' ? `${targetAlliance} ` : ''}tokens ${inCombat ? 'in combat' : 'on the scene'}.`);
 }
 async function clearSelection() {
     await canvas.tokens.releaseAll();
@@ -66,21 +67,21 @@ tokenActionDialog = await foundry.applications.api.DialogV2.wait({
     form: {
         closeOnSubmit: false
     },
-	rejectClose: false,
+    rejectClose: false,
     content: `
     <style>
       .toggle-button-container {
         display: flex;
         margin-top: -5px;
         margin-bottom: -5px;
-      }
+        }
       .toggle-button {
         filter: brightness(0.7);
         gap: 5px;
       }
       .toggle-button.active {
         filter: brightness(1);
-      }
+        }
     </style>
     <div class="toggle-button-container">
       <button type="button" class="toggle-button active" id="scene-button" data-value="scene"><i class="fas fa-map"></i> In Scene</button>
@@ -89,6 +90,7 @@ tokenActionDialog = await foundry.applications.api.DialogV2.wait({
     </div>
     <div class="toggle-button-container">
       <button type="button" class="toggle-button active" id="party-button" data-value="party"><i class="fas fa-users"></i> Party</button>
+	  <button type="button" class="toggle-button" id="both-button" data-value="both"><i class="fas fa-project-diagram"></i> Both</button>
       <button type="button" class="toggle-button" id="opposition-button" data-value="opposition"><i class="fas fa-skull"></i> Opposition</button>
       <input type="hidden" id="target" name="target" value="party">
     </div>
@@ -125,28 +127,34 @@ tokenActionDialog = await foundry.applications.api.DialogV2.wait({
         const sceneButton = html.querySelector('#scene-button');
         const combatButton = html.querySelector('#combat-button');
         const partyButton = html.querySelector('#party-button');
+        const bothButton = html.querySelector('#both-button');
         const oppositionButton = html.querySelector('#opposition-button');
         const scopeInput = html.querySelector('#scope');
         const targetInput = html.querySelector('#target');
 
-        function setToggleButtonState(activeButton, inactiveButton, value, hiddenInput) {
+        function setToggleButtonState(activeButton, inactiveButtons, value, hiddenInput) {
             activeButton.classList.add('active');
             activeButton.style.filter = 'brightness(1)';
-            inactiveButton.classList.remove('active');
-            inactiveButton.style.filter = 'brightness(0.7)';
+            inactiveButtons.forEach(button => {
+                button.classList.remove('active');
+                button.style.filter = 'brightness(0.7)';
+            });
             hiddenInput.value = value;
         }
         sceneButton.addEventListener('click', () => {
-            setToggleButtonState(sceneButton, combatButton, 'scene', scopeInput);
+            setToggleButtonState(sceneButton, [combatButton], 'scene', scopeInput);
         });
         combatButton.addEventListener('click', () => {
-            setToggleButtonState(combatButton, sceneButton, 'combat', scopeInput);
+            setToggleButtonState(combatButton, [sceneButton], 'combat', scopeInput);
         });
         partyButton.addEventListener('click', () => {
-            setToggleButtonState(partyButton, oppositionButton, 'party', targetInput);
+            setToggleButtonState(partyButton, [oppositionButton, bothButton], 'party', targetInput);
         });
         oppositionButton.addEventListener('click', () => {
-            setToggleButtonState(oppositionButton, partyButton, 'opposition', targetInput);
+            setToggleButtonState(oppositionButton, [partyButton, bothButton], 'opposition', targetInput);
+        });
+        bothButton.addEventListener('click', () => {
+            setToggleButtonState(bothButton, [partyButton, oppositionButton], 'both', targetInput);
         });
         const buttonsContainer = html.querySelector(".form-footer");
         if (buttonsContainer) {
